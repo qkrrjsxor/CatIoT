4283190dde3bbd3462f69ea15fd686ad259217d4
qkrrjsxor <qkrrjsxor12@gmail.com>
2024-07-12 16:54:10 +0900

    Feat: DB Schema 초안

---
+++ Backend/DB_schema.sql
@@ -0,0 +1,47 @@
+CREATE SCHEMA IF NOT EXISTS CIoT;
+
+USE CIoT;
+
+-- user table
+CREATE TABLE IF NOT EXISTS user(
+	user_id VARCHAR(20) PRIMARY KEY,
+    password VARCHAR(50) NOT NULL,
+    user_name VARCHAR(20) NOT NULL
+);
+
+-- cat table
+CREATE TABLE IF NOT EXISTS cat(
+	cat_id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id varchar(20) NOT NULL,
+    cat_name varchar(20) NOT NULL,
+    cat_age INT NOT NULL,
+    cat_gender ENUM('male', 'female') NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+-- meal schedule table
+CREATE TABLE IF NOT EXISTS meal_schedule(
+	schedule_id INT AUTO_INCREMENT PRIMARY KEY,
+    cat_id INT NOT NULL,
+    meal_time TIME NOT NULL,
+    meal_amount INT NOT NULL,
+    FOREIGN KEY (cat_id) REFERENCES cat(cat_id)
+);
+
+-- meal eaten table
+CREATE TABLE IF NOT EXISTS meal_eaten(
+	meal_id INT AUTO_INCREMENT PRIMARY KEY,
+    cat_id INT NOT NULL,
+    meal_date TIMESTAMP NOT NULL,
+    meal_amount INT NOT NULL,
+	FOREIGN KEY (cat_id) REFERENCES cat(cat_id)
+);
+
+-- toilet table
+CREATE TABLE IF NOT EXISTS toilet(
+	toilet_id INT AUTO_INCREMENT PRIMARY KEY,
+    cat_id INT NOT NULL,
+    toilet_time TIMESTAMP NOT NULL,
+    toilet_type ENUM('pee', 'poo'),
+    FOREIGN KEY (cat_id) REFERENCES cat(cat_id)
+);
