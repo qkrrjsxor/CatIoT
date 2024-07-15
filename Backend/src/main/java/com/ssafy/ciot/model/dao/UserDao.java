1a978013c47fb3833efd1b32294330bad249487d
qkrrjsxor <qkrrjsxor12@gmail.com>
2024-07-15 15:14:06 +0900

    Feat: Mybatis 연결, 회원가입, 로그인, 로그아웃 구현 User Dto, Dao, Service, Controller 작성 그 외 dto 작성

--- Backend/src/main/java/com/ssafy/ciot/model/dao/UserDao.java
+++ Backend/src/main/java/com/ssafy/ciot/model/dao/UserDao.java
@@ -1,5 +1,11 @@
 package com.ssafy.ciot.model.dao;
 
-public class UserDao {
+import com.ssafy.ciot.model.dto.User;
 
+public interface UserDao {
+	// 회원가입
+	public int insertUser(User user);
+
+	// 로그인
+	public User selectUser(String userId);
 }
