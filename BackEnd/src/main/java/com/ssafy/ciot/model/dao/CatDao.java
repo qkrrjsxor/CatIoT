4f616626ba25fead40fb0552c8e28f900a12fd63
qkrrjsxor <qkrrjsxor12@gmail.com>
2024-07-18 10:39:19 +0900

    Feat: 회원가입시 cat 정보 등록 추가

---
+++ BackEnd/src/main/java/com/ssafy/ciot/model/dao/CatDao.java
@@ -0,0 +1,5 @@
+package com.ssafy.ciot.model.dao;
+
+public class CatDao {
+
+}
--- BackEnd/src/main/java/com/ssafy/ciot/model/service/UserService.java
+++ BackEnd/src/main/java/com/ssafy/ciot/model/service/UserService.java
@@ -4,28 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.ciot.model.dao.UserDao;
+import com.ssafy.ciot.model.dto.Cat;
 import com.ssafy.ciot.model.dto.User;
 
 @Service
 public class UserService {
-	
+
 	private final UserDao userDao;
-	
+
 	@Autowired
-	public UserService (UserDao userDao) {
+	public UserService(UserDao userDao) {
 		this.userDao = userDao;
 	}
-	
+
 	// 회원 가입
-	public int signup(User user) {
-		return userDao.insertUser(user);
+	public int signup(User user, Cat cat) {
+		int userResult = 0;
+		userResult = userDao.insertUser(user);
+
+		int catResult = 0;
+		cat.setUserId(user.getUserId());
+		catResult = userDao.insertCat(cat);
+
+		return userResult*catResult;
+
 	}
-	
+
 	// 로그인
 	public User login(String userId) {
 		return userDao.selectUser(userId);
 	}
-	
+
 	// 조회
 	public User findByUserId(String userId) {
 		return userDao.selectUser(userId);
