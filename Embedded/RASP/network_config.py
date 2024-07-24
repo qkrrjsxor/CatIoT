5f256bd425ef6cf80f24c4c44844b4117e0a75c6
ParkSeungHeun <qkrtmdgjs12@naver.com>
2024-07-24 14:27:20 +0900

    Feat: 배급 간격 조회 및 배급 데이터 기반 STM 명령 기능 구현

--- Embedded/RASP/network_config.py
+++ Embedded/RASP/network_config.py
@@ -1,10 +1,14 @@
 # RASP
-RASP_IP = '192.168.165.161'
+RASP_IP = '192.168.4.2'
 PORT = 8080
+
+# 수동 배급 요청 및
+SERVER_COMMUNICATION_PORT = 50000
 
 # SERVER
 # meal
 SERVER_IP = "70.12.246.104"
 MEAL_URL = f"http://{SERVER_IP}:8080/api/meal/eaten"
+MEAL_SCHEDULE_URL = f"http://{SERVER_IP}:8080/api/meal/getschedule/1"
 
 # toilet
