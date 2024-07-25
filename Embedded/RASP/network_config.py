038964b9b2ae3acccde2398a3abe0d53427da034
ParkSeungHeun <qkrtmdgjs12@naver.com>
2024-07-25 13:52:31 +0900

    Feat: 서버로 부터 수동배급 신호 수신 시 STM에 배급 신호 전송, 동작 확인까지 완료

--- Embedded/RASP/network_config.py
+++ Embedded/RASP/network_config.py
@@ -5,7 +5,7 @@ PORT = 8080
 # 수동 배급 요청 및 서버 신호 전용 포트
 SERVER_COMMUNICATION_PORT = 50000
 
-# SERVER
+# SERVER IP
 # meal
 SERVER_IP = "70.12.246.104"
 MEAL_URL = f"http://{SERVER_IP}:8080/api/meal/eaten"
