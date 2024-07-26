350c49e3a222c47090fef3b5f6624ac2ff2b6f14
qkrrjsxor <qkrrjsxor12@gmail.com>
2024-07-26 16:18:20 +0900

    Modify: BackendApplicationTests annotation

--- BackEnd/src/test/java/com/ssafy/BackendApplicationTests.java
+++ BackEnd/src/test/java/com/ssafy/BackendApplicationTests.java
@@ -3,7 +3,9 @@ package com.ssafy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.ssafy.ciot.BackendApplication;
+
+@SpringBootTest(classes = BackendApplication.class)
 class BackendApplicationTests {
 
 	@Test
